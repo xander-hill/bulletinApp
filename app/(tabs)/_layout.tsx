ad979e6f0f91a8d50dce7761d21c008da2c3c786
Xander Hill <xander.k.hill@gmail.com>
2025-06-27 13:46:29 -0500

    new icon for search tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -57,7 +57,7 @@ export default function TabLayout() {
         name="search"
         options={{
           title: 'Search',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="plus.circle.fill" color={color} />, 
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="magnifyingglass" color={color} />, 
         }}
       />
       <Tabs.Screen
