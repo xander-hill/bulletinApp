f2be8520f89d1bda60cced0a52abd71b860b3a96
Xander Hill <xander.k.hill@gmail.com>
2025-07-14 14:52:08 -0500

    map in corner, slightly larger

--- components/HomeFeed.tsx
+++ components/HomeFeed.tsx
@@ -98,11 +98,11 @@ export default function HomeFeed() {
 const styles = StyleSheet.create({
   miniMapContainer: {
     position: 'absolute',
-    top: 40,
-    right: 20,
-    width: 120,
-    height: 120,
-    borderRadius: 60,
+    top: 0,
+    right: 0,
+    width: 180, // increased from 120
+    height: 180, // increased from 120
+    borderRadius: 90, // still circular for now
     overflow: 'hidden',
     borderWidth: 2,
     borderColor: '#fff',
